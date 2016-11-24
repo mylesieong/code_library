77855112cfb521fb15ea41969103d515092265b2
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-11-24 15:38:46 +0800

    Explicit Commit

--- @commit_n_push.bat
+++ @commit_n_push.bat
@@ -1,1 +1,1 @@
-git status >> ./push.log && git add * >> ./push.log && git commit -m "Explicit Commit" >> ./push.log && git push origin master >> ./push.log && exit
+git add * >> ./push.log && git commit -m "Explicit Commit" >> ./push.log && git push origin master >> ./push.log && exit
